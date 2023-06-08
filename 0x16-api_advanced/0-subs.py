73f65bcde8b3d22373436e80904e74831d723757
1LolaOla <funmiolay1@gmail.com>
2023-06-08 19:17:21 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,9 +15,9 @@ def number_of_subscribers(subreddit):
     If the subreddit is invalid, returns 0.
     """
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "user_agent"}
-    response = requests.get(
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(   
         url=url,
         headers=headers,
         allow_redirects=False)
